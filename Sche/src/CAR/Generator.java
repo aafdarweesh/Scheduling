cd66a17c9eba1a59340bb3adb73cf72b1bdbcdf5
aafdarweesh <aafdarweesh@gmail.com>
2018-04-19 21:34:25 +0300

    Update Generator.java
    updated v4

--- Sche/src/CAR/Generator.java
+++ Sche/src/CAR/Generator.java
@@ -12,14 +12,21 @@ public class Generator {
 	ArrayList<Integer> serviceTimeForEachPacket;
 	ArrayList<Integer> arrivalRate;
 	ArrayList<Integer> arrivalTime;
+	int numberOfNodes;
+	GeneralInfo general;
+	Server server;
 
-	Generator() {
+	Generator(GeneralInfo g, Server s) {
+		general = g;
 		genaratedPackets = new ArrayList<Packet>();
 		numOfPacketList = new ArrayList<Integer>();
 		serviceTimeForEachPacket = new ArrayList<Integer>();
 		arrivalRate = new ArrayList<Integer>();
 		arrivalTime = new ArrayList<Integer>();
 		totalNumOfPackets = 0;
+		numOfpacketType = 0;
+		numberOfNodes = 0;
+		server = s;
 	}
 
 	public static int getPoissonRandom(double mean) {
@@ -100,6 +107,8 @@ public class Generator {
 		int i = 0;
 		int count = 0;
 
+		int idcounter = 0;
+
 		/* saving the vauls inside the array */
 		while (j < numOfpacketType) {
 			while (count < numOfPacketList.get(j)) {
@@ -107,34 +116,35 @@ public class Generator {
 				Random rand = new Random();
 				Random rand2 = new Random();
 
-				int randomNum = rand.nextInt((10 - 1) + 1) + 1;
-				int randomNum2 = rand2.nextInt((10 - 1) + 1) + 1;
+				int randomNum = rand.nextInt((general.allNodes.size() - 1) + 1) + 1;
+				int randomNum2 = rand2
+						.nextInt((general.allNodes.size() - 1) + 1) + 1;
 
 				destination = randomNum;
 				source = randomNum2;
 				while (source == destination) {
-					randomNum = rand.nextInt((10 - 1) + 1) + 1;
+					randomNum = rand.nextInt((general.allNodes.size() - 1) + 1) + 1;
 					destination = randomNum;
 				}
 
-				Packet p = new Packet();
+				Packet p = new Packet(j + 1, destination, source, 0,
+						serviceTimeForEachPacket.get(j), 2 * j, idcounter++);
 
-				p.destination = destination;
-				p.source = source;
-				p.serviceTime = serviceTimeForEachPacket.get(j);
-				p.type = j + 1;
-				;
+				/*
+				 * p.destination = destination; p.source = source; p.serviceTime
+				 * = serviceTimeForEachPacket.get(j); p.type = j + 1;
+				 */
 				genaratedPackets.add(p);
 
 				count++;
-				i++;
+				// i++;
 			}
 
 			j++;
 			count = 0;
 
 		}
-		for (i = 0; i < totalNumOfPackets; i++) // reorgamizing the events
+		for (i = 0; i < totalNumOfPackets; i++) // reorganizing the events
 												// randomly
 		{
 			for (j = 0; j < totalNumOfPackets; j++) {
@@ -143,11 +153,8 @@ public class Generator {
 
 				int randIndex = rand.nextInt(totalNumOfPackets);
 
-				Packet temp = new Packet();
-				Packet temp2 = new Packet();
-
-				temp = genaratedPackets.get(i);
-				temp2 = genaratedPackets.get(randIndex);
+				Packet temp = genaratedPackets.get(i);
+				Packet temp2 = genaratedPackets.get(randIndex);
 
 				genaratedPackets.remove(i);
 				genaratedPackets.add(i, temp2);
@@ -167,12 +174,47 @@ public class Generator {
 		}
 	}
 
+	public void GenerateNodes() {
+
+		System.out.print("\nEnter the number of nodes :");
+		Scanner in = new Scanner(System.in);
+
+		numberOfNodes = in.nextInt();
+
+		for (int i = 0; i < numberOfNodes; ++i) {
+			Node node = new Node(i, server, general);
+			general.allNodes.add(node);
+		}
+
+	}
+
+	// this function will define the connection between the nodes
+	public void GenerateConnectionBetweenNodes() {
+		int n = general.allNodes.size();
+		general.nodesConnections = new int[n][n];
+		Random rand = new Random();
+		int rando;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+
+				if (i == j || general.nodesConnections[i][j] == 1)
+					continue;
+				general.nodesConnections[i][j] = general.nodesConnections[j][i] = 0;
+				rando = rand.nextInt(50) + 1;
+				if (rando > 35)
+					general.nodesConnections[i][j] = general.nodesConnections[j][i] = 1;
+			}
+		}
+	}
+
 	void Run() {
 
 		storeNumberofPacket();
 		storeServiceTimeForPacket();
 		storeArrivalRate();
 		storeGenaratedPackets();
+		GenerateConnectionBetweenNodes();
+		GenerateNodes();
 
 	}
 
