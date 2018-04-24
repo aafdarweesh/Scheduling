79dfc148430c58214d65f1002fafaf3fe99f4d52
aafdarweesh <aafdarweesh@gmail.com>
2018-04-25 01:46:12 +0300

    Update GeneralInfo.java
    Test 2

--- Sche/src/CAR/GeneralInfo.java
+++ Sche/src/CAR/GeneralInfo.java
@@ -5,27 +5,20 @@ import java.util.Random;
 
 public class GeneralInfo {
 	int timeCounter;
-	int numberOfDroppedPackets; // number of packets that have been dropped
-	int rerequestedPackets; // number of re-requested packets (should equal the
-							// number of dropped packets
-	int limitedTimeForPacket; // the maximum time the packet can take in the
-								// system before requesting sending the packet
-								// again
-	int packetDeliveredMoreThanOneTime; // the number of packets delivered more
-										// than one time
+	int numberOfDroppedPackets; 
+	int rerequestedPackets; 
+	int limitedTimeForPacket; 
+	int packetDeliveredMoreThanOneTime; 
 
 	ArrayList<Node> allNodes;
-	ArrayList<Packet> delivered; // list of delivered packets
+	ArrayList<Packet> delivered; 
 	ArrayList<Packet> dropped;
-	int[][] nodesConnections; //the links between the nodes
-
+	int[][] nodesConnections; 
 	GeneralInfo() {
 		this.timeCounter = 0;
 		this.numberOfDroppedPackets = 0;
 		this.rerequestedPackets = 0;
-		this.limitedTimeForPacket = 20; // the maximum time the packet can take
-										// in the system before requesting
-										// another packet
+		this.limitedTimeForPacket = 20; 
 		this.packetDeliveredMoreThanOneTime = 0;
 
 		this.allNodes = new ArrayList<Node>();
@@ -35,51 +28,16 @@ public class GeneralInfo {
 	}
 
 	public boolean CheckNodeStatus(int nodeId) {
-		return this.allNodes.get(nodeId).failure;
+		return this.allNodes.get(nodeId).notfailure;
 	}
-/*
-	public void PushToNodeBuffer(int nodeId, Packet newPacket) {
-		allNodes.get(nodeId).buffer.Push(this,newPacket);
-	}
-*/
+
 	public void addToNodeExpected(Packet newPacket) {
 		int destination;
 		destination = newPacket.destination;
 		allNodes.get(destination).expectedPackets.add(newPacket);
 	}
 	
-	/*
-	public void Run(Generator generator){
-		//genaratedPackets
-		this.timeCounter++; //change the current time
-		
-		
-		
-		for(int i = 0; i < generator.totalNumOfPackets; ++i){
-			Packet currentPacket = generator.genaratedPackets.get(i);
-			if(currentPacket.arrivelTime == this.timeCounter){
-				
-				int source = currentPacket.source;
-				//int destination = currentPacket.destination;
-				
-				boolean checkIfFullBuffer = this.allNodes.get(source).buffer.full();
-				if(checkIfFullBuffer == false){
-					this.allNodes.get(source).buffer.packetQueue.add(currentPacket);
-					addToNodeExpected(currentPacket);
-				}else{
-					currentPacket.arrivelTime++;
-				}
-			}
-		}
-		
-		
-		for(int i = 0; i < allNodes.size(); ++i){
-			allNodes.get(i).Run();
-		}
-		
-		
-	}
-	*/
+
 	public void printToSystem()
 	{
 		System.out.println("//////////////////////////////////////");
@@ -87,7 +45,7 @@ public class GeneralInfo {
 		System.out.println("//////////////////////////////////////");
 		for (int i=0;i<allNodes.size();++i)
 		{
-			System.out.println("Node : "+ allNodes.get(i).id + ", Not Failure  :" + allNodes.get(i).failure + ", The status : "+ allNodes.get(i).status);
+			System.out.println("Node : "+ allNodes.get(i).id + ", Not Failure  :" + allNodes.get(i).notfailure + ", The status : "+ allNodes.get(i).status);
 		
 			 System.out.println("the buffer content   ");
 			 allNodes.get(i).buffer.print();
@@ -108,7 +66,7 @@ public class GeneralInfo {
 		System.out.println("limitedTimeForPacket : " + limitedTimeForPacket);
 		System.out.println("packetDeliveredMoreThanOneTime : " + packetDeliveredMoreThanOneTime);
 		
-		System.out.println("List of Delievered packets : ");
+		System.out.println("List of Delievered packets : " + this.delivered.size());
 		for(int i = 0 ; i < delivered.size(); ++i){
 			System.out.print("ID "+delivered.get(i).id);
 			System.out.print(", Type "+delivered.get(i).type);
